1a21881493e07e762db445122444aaef34e96397
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2019-11-27 14:26:56 +0000

    initial twilio sms send PoC

--- wishes/views.py
+++ wishes/views.py
@@ -1,7 +1,45 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from twilio.rest import Client
+from django.conf import settings
 from .models import Wish
 from .serializers import WishSerializer
+
+class TwilioSmsSender:
+    '''
+    Wrapper class to test/send sms using the Twilio SMS API
+    '''
+
+    def __init__(self, text, to_mobile):
+        self._text = text
+        self._to_mobile = to_mobile
+
+    
+    def send_sms(self):
+        account_sid = settings.TWILIO_ACCOUNT_SID
+        auth_token = settings.TWILIO_AUTH_TOKEN
+        client = Client(account_sid, auth_token)
+
+        message = client.messages.create(
+            body=self._text,
+            to = self._to_mobile,
+            from_ = settings.TWILIO_FROM
+        )
+        return message.sid
+        
+
+class SendSMS(APIView):
+
+    def post(self, request):
+        try:
+            twilio = TwilioSmsSender(request.data.get('text'), request.data.get('mobile'))
+            info = twilio.send_sms()
+            return Response({'message': 'sms sent', 'info': info})
+        except Exception as ex:
+            return Response({'message': 'sms not sent', 'info': str(ex)}, status=402)
+
 
 
 class WishesIndex(ListCreateAPIView):
